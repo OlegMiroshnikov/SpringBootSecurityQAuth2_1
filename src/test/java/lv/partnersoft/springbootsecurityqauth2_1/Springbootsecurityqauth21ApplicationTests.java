283638e424daafc6b7b35380fa82202206002a25
Oleg Miroshnikov <oleg.psoft@gmail.com>
2020-11-17 14:35:34 +0200

    Initial commit

---
+++ src/test/java/lv/partnersoft/springbootsecurityqauth2_1/Springbootsecurityqauth21ApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.partnersoft.springbootsecurityqauth2_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootsecurityqauth21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
